c450fea89bbb33982bec157f1df171ca8dd9b061
Jeffrey Chuc <jeffreychuc@gmail.com>
2017-11-15 14:26:55 -0800

    Add routes

---
+++ pokedex/app/controllers/api/pokemon_controller.rb
@@ -0,0 +1,7 @@
+class Api::PokemonController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- pokedex/config/routes.rb
+++ pokedex/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  # Your routes here!
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, default: { format: :json } do 
+    resources :pokemon, only: [:index, :create, :show, :update, :destroy]
+  end
+
+
+  root to: 'static_pages#root'
 end
