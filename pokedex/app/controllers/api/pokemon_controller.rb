3d2a3073d0577d28e0f27111f434c29fbbc8382e
Jeffrey Chuc <jeffreychuc@gmail.com>
2017-11-15 14:30:34 -0800

    add api::pokemoncontroller

--- pokedex/app/controllers/api/pokemon_controller.rb
+++ pokedex/app/controllers/api/pokemon_controller.rb
@@ -1,7 +1,11 @@
 class Api::PokemonController < ApplicationController
   def index
+    @Pokemon = Pokemon.all
+    render :index
   end
 
   def show
+    @Pokemon = Pokemon.find_by(id: params[:id])
+    render :show
   end
 end
